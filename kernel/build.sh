ed78409766d1ff56d08ee1afd3868dbf606a08b4
Tritax <feltiatix@gmail.com>
2016-10-08 13:46:54 -0400

    updated to make, reworked object files to subdir, rewired build.sh to call make

--- kernel/build.sh
+++ kernel/build.sh
@@ -1,7 +1,8 @@
 #
-cd ./src
+#cd ./src
 
 # -s -ffunction-sections -fdata-sections -Wl,--gc-sections
-gcc -m32 -s -ffreestanding -nostartfiles -nostdlib -lgcc stdio.cpp entry.cpp main.cpp -o ../bin/kernel.sys -O2 -I ../include -Wl,-e,kernel_entry -T ../link.ld
+#gcc -m32 -s -ffreestanding -nostartfiles -nostdlib -lgcc stdio.cpp entry.cpp main.cpp -o ../bin/kernel.sys -O2 -I ../include -Wl,-e,kernel_entry -T ../link.ld
 
-cp ../bin/kernel.sys ../../bin/kernel.sys
+#cp ../bin/kernel.sys ../../bin/kernel.sys
+make
